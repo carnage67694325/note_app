eaa67648e27f6d1e1f333635a8995312ed541b04
carnage67694325 <kingabdoeid2014@gmail.com>
2024-08-31 02:51:21 +0300

    changed note icon

--- lib/views/widget/home_view_body.dart
+++ lib/views/widget/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:note_app/cubits/note_cubit/notes_cubit.dart';
 import 'package:note_app/views/widget/custom_app_bar.dart';
 import 'package:note_app/views/widget/notes_list.dart';
@@ -30,7 +31,7 @@ class _HomeViewBodyState extends State<HomeViewBody> {
             ),
             CustomAppBar(
               title: 'Notes',
-              icon: Icons.search,
+              icon: FontAwesomeIcons.noteSticky,
             ),
             Expanded(child: NotesList())
           ],
